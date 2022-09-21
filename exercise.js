bf1a035e39c864600067340fcb0fc9f61aecdb1f
Flavius <flaviusbejan96@yahoo.com>
2022-09-21 14:02:22 +0300

    Finish

---
+++ exercise.js
@@ -0,0 +1,22 @@
+function canPlay() {
+  let sport = " Football";
+
+  if (true) {
+    let personName = "Cosimo";
+    console.log(personName + sport);
+  }
+
+  //console.log(personName + sport);
+}
+
+canPlay();
+
+/* This is a function scope, 
+variables declare inside a
+function is not accessible
+ from outside the function,
+ I solve the function to run,
+ but I didn't
+ understand well, So I will
+ continue to study to
+ understand better*/
